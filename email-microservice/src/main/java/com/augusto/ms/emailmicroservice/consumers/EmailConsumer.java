583cdab0630e80cb0ecff94ac47121cb0c912337
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-14 22:01:49 -0300

    feat: update consumer and rabbitmq configuration

--- email-microservice/src/main/java/com/augusto/ms/emailmicroservice/consumers/EmailConsumer.java
+++ email-microservice/src/main/java/com/augusto/ms/emailmicroservice/consumers/EmailConsumer.java
@@ -1,5 +1,6 @@
 package com.augusto.ms.emailmicroservice.consumers;
 
+import com.augusto.ms.emailmicroservice.dtos.EmailRecordDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,8 @@ import org.springframework.stereotype.Component;
 public class EmailConsumer {
 
   @RabbitListener(queues = "${broker.queue.email.name}")
-  public void listenEmailQueue(@Payload String string) {
-    System.out.println(string);
+  public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+    System.out.println(emailRecordDto.emailTo());
   }
   
 }

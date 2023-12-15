1ea371f50d34c393f9ed944100efb4036eb431ee
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-15 00:20:59 -0300

    feat: implementing email sending logic

--- email-microservice/src/main/java/com/augusto/ms/emailmicroservice/consumers/EmailConsumer.java
+++ email-microservice/src/main/java/com/augusto/ms/emailmicroservice/consumers/EmailConsumer.java
@@ -1,16 +1,30 @@
 package com.augusto.ms.emailmicroservice.consumers;
 
 import com.augusto.ms.emailmicroservice.dtos.EmailRecordDto;
+import com.augusto.ms.emailmicroservice.models.EmailModel;
+import com.augusto.ms.emailmicroservice.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+  final EmailService emailService;
+
+  public EmailConsumer(EmailService emailService) {
+    this.emailService = emailService;
+  }
+
   @RabbitListener(queues = "${broker.queue.email.name}")
   public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
-    System.out.println(emailRecordDto.emailTo());
+    var emailModel = new EmailModel();
+    BeanUtils.copyProperties(emailRecordDto, emailModel);
+    // Send email
+    emailService.sendEmail(emailModel);
+
   }
   
 }
